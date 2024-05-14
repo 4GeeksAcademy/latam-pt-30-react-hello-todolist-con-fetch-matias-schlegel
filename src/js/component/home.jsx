46f3186e6aa8ba2e8139dfd5b78df99736fb2129
matischlegel <161637922+matischlegel@users.noreply.github.com>
2024-05-14 03:29:03 +0000

    empezando

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [firstSubmit, setFirstSubmit] = useState("")
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<React.Fragment>
+			<div className="container vh-100">
+				<h1>todos</h1>
+				<h1>{firstSubmit}</h1>
+					<div className="list">
+						<ul>
+							<li>
+								<input 
+								type="text" 
+								placeholder="What needs to be done?"
+								onChange={(event) => {
+									setFirstSubmit(event.target.value)
+								}}
+								value={firstSubmit}
+								onKeyDown={(event) => {
+									if (event.key == "Enter") {
+										setFirstSubmit("")
+									}
+								}}
+								/>
+							</li>
+							<li>hola <i class="fa fa-times"></i> </li>
+						</ul>
+					</div>
+					<button
+						type="reset"
+						onClick={(event) => {
+							setFirstSubmit("")
+						}}
+					>X</button>
+			</div>
+		</React.Fragment>
 	);
 };
 
